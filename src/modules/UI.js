f887f7303641a7dec3e15c3c5542bd8c84ab6a8b
Kenny <Kenny.graczyk2@gmail.com>
2023-03-10 22:57:19 +0900

    refactor svg logic

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,5 +1,4 @@
 import { Project } from './Projects'
-import PlusCircle from '../../img/plus-circle.svg'
 
 export const UI = (() => {
     function renderHeader() {
@@ -24,7 +23,11 @@ export const UI = (() => {
             <div class="sidebar-items">
                 <div id="projects-bar" class="projects-bar">
                     <p class="sidebar-item">Projects</p>
-                    <img class="add-project-button" id="add-project-button" src=${PlusCircle}/>
+                    <svg class="add-project-button" id="add-project-button" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus-circle">
+                        <circle cx="12" cy="12" r="10"></circle>
+                        <line x1="12" y1="8" x2="12" y2="16"></line>
+                        <line x1="8" y1="12" x2="16" y2="12"></line>
+                    </svg>
                 </div>
                 <div class="project-list" id="project-list">
                 </div>

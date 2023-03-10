070b2a113faa17e653d1f18963179217e97fbbfd
Kenny <Kenny.graczyk2@gmail.com>
2023-03-10 22:27:53 +0900

    Initial dist subtree commit

---
+++ main.js
@@ -0,0 +1,23 @@
+(self["webpackChunktodo"] = self["webpackChunktodo"] || []).push([["main"],{
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+// src/index.js
+const test = () => {
+    console.log( 'test' );
+};
+test();
+
+/***/ })
+
+},
+/******/ __webpack_require__ => { // webpackRuntimeModules
+/******/ var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+/******/ var __webpack_exports__ = (__webpack_exec__("./src/index.js"));
+/******/ }
+]);
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWFpbi5qcyIsIm1hcHBpbmdzIjoiOzs7Ozs7OztBQUFBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly90b2RvLy4vc3JjL2luZGV4LmpzIl0sInNvdXJjZXNDb250ZW50IjpbIi8vIHNyYy9pbmRleC5qc1xuY29uc3QgdGVzdCA9ICgpID0+IHtcbiAgICBjb25zb2xlLmxvZyggJ3Rlc3QnICk7XG59O1xudGVzdCgpOyJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
